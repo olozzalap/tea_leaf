252afec27428e2884d80033e552a71f1aa71f5e0
Eben Palazzolo <olozzalap@gmail.com>
2013-05-16 17:26:45 -0400

    blackjack_oo.rb update

--- blackjack_oo.rb
+++ blackjack_oo.rb
@@ -1,12 +1,28 @@
 class cards
 	@@deck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 'a', 'a', 'a', 'a']
 
+  def hit
+    @@deck.shuffle!
+
+    if @@deck[0] == 'a'
+		aces += 1
+	elsif @@deck[0] != 'a'
+		non_aces += @@deck[0]
+	end
+	@@deck.delete_at(0)
+  end
+
+  def start
+    hit
+	hit
+  end
 
 end
 
 class dealer < cards
 	attr_accessor: non_aces
 	attr_accessor: aces
+	attr_accessor: ideal
 	attr_accessor: hand
 
 	def initialize
@@ -14,35 +30,26 @@ class dealer < cards
 		@aces = 0
 		@hand = 0
 		@name = "The Dealer"
+		@ideal = 0
 	end
 
-	def start
-		@@deck.shuffle!
 
-		if @@deck[0] == 'a'
-			aces += 1
-		elsif @@deck[1] == 'a'
-			aces += 1
-		end
-
-		if @@deck[0] != 'a'
-			non_aces += @@deck[0]
-		elsif @@deck[1]
-			non_aces += @@deck[1]
-		end
-			@@deck.delete_at(0)
-			@@deck.delete_at(1)	
-	end
-
-  def hit
-    @@deck.shuffle!
-
-    if @@deck[0] == 'a'
-			aces += 1
-		elsif @@deck[0] != 'a'
-			non_aces += @@deck[0]
-		end
-		@@deck.delete_at(0)
+  
+  def ideal_hand
+    if non_aces + aces > 21
+	  bust
+	elsif aces == 0
+	  non_aces
+	elsif non_aces <= 10 && aces == 1
+	  non_aces + 11
+	elsif non_aces <= 9 && aces == 2
+	  non_aces + 11 + 1
+  	elsif non_aces <= 8 && aces == 3
+	  non_aces + 11 + 1 + 1
+  	elsif non_aces <= 7 && aces == 4
+	  non_aces + 11 + 1 + 1 + 1
+	elsif non_aces > 10
+	  non_aces + aces
   end
 
   if hand < 17
@@ -66,35 +73,25 @@ class player < cards
 		print "> "
 		@name = gets.chomp
 	end
-
-	def start
-		@@deck.shuffle!
-
-		if @@deck[0] == 'a'
-			aces += 1
-		elsif @@deck[1] == 'a'
-			aces += 1
-		end
-
-		if @@deck[0] != 'a'
-			non_aces += @@deck[0]
-		elsif @@deck[1]
-			non_aces += @@deck[1]
-		end
-			@@deck.delete_at(0)
-			@@deck.delete_at(1)	
+	
+	def check_hand
+	  puts "You have #{non_aces} and #{aces} aces"
+	  if non_aces + aces > 21
+	    bust
+	  elsif aces == 0
+	    non_aces
+	  elsif non_aces <= 10 && aces == 1
+	    non_aces + 11
+	  elsif non_aces <= 9 && aces == 2
+	    non_aces + 11 + 1
+      elsif non_aces <= 8 && aces == 3
+	    non_aces + 11 + 1 + 1
+      elsif non_aces <= 7 && aces == 4
+	    non_aces + 11 + 1 + 1 + 1
+	  elsif non_aces > 10
+	    non_aces + aces
+	  end
 	end
-
-  def hit
-    @@deck.shuffle!
-
-    if @@deck[0] == 'a'
-			aces += 1
-		elsif @@deck[0] != 'a'
-			non_aces += @@deck[0]
-		end
-		@@deck.delete_at(0)
-  end
   
   @lowest = non_aces + aces
   @highest = non_aces + (aces * 11)
